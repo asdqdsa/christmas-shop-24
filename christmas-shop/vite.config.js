c8755d7e36f01a4daa2a093e9df3e379a97c01e9
asdqdsa <asdqdsa@protonmail.com>
2024-11-12 17:24:40 +0400

    init: set bundler up

---
+++ christmas-shop/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/asdqdsa-JSFE2024Q4/',
+  build: {
+    sourcemap: true, // Enables source maps for prod build
+    minify: false, // Disable minification for prod build
+  },
+});
