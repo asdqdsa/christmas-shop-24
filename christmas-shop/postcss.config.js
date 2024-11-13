761228bcd3ae835773ea6495fd1deefadd6a8b84
asdqdsa <asdqdsa@protonmail.com>
2024-11-13 15:59:43 +0400

    build: switch postcss.config.js to ES module, add missing package

--- christmas-shop/postcss.config.js
+++ christmas-shop/postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+import autoprefixer from "autoprefixer";
+import postcssNested from "postcss-nested";
+
+export default {
   plugins: {
     autoprefixer: {},
-    'postcss-nested': {},
+    "postcss-nested": {},
   },
-}
+};
