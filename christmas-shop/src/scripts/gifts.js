3fe5297d7d050a0daba36255b975723864ef186f
asdqdsa <asdqdsa@protonmail.com>
2024-11-29 16:17:55 +0400

    fix: move scroll-to-top to gifts page

--- christmas-shop/src/scripts/gifts.js
+++ christmas-shop/src/scripts/gifts.js
@@ -1,1 +1,18 @@
-console.log("gifts is set");
+console.log('gifts is set');
+
+// Scroll to Top
+const scrollUpArrow = document.querySelector('#scroll-up');
+scrollUpArrow.addEventListener('click', handleScrollToTop);
+
+function handleScrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollUpArrow.classList.remove('scroll-up-arrow_inactive');
+  } else scrollUpArrow.classList.add('scroll-up-arrow_inactive');
+});
--- christmas-shop/src/scripts/main.js
+++ christmas-shop/src/scripts/main.js
@@ -35,23 +35,6 @@ function updateLayoutTimer(targetDate) {
   seconds.textContent = getData.secondsDelta;
 }
 
-// Scroll to Top
-const scrollUpArrow = document.querySelector('#scroll-up');
-scrollUpArrow.addEventListener('click', handleScrollToTop);
-
-function handleScrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    scrollUpArrow.classList.remove('scroll-up-arrow_inactive');
-  } else scrollUpArrow.classList.add('scroll-up-arrow_inactive');
-});
-
 // TODO Slider
 
 // TODO Menu
