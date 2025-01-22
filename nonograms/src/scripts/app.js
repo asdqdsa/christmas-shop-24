17d593a6d86ed94e97bd4db20c003e512ec0b6ad
asdqdsa <asdqdsa@protonmail.com>
2025-01-22 11:16:34 +0400

    init: nonograms dir

---
+++ nonograms/src/scripts/app.js
@@ -0,0 +1,4 @@
+import './store';
+import './view';
+
+console.log('app is set');
---
+++ nonograms/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+
+export default defineConfig({
+  base: '/asdqdsa-JSFE2024Q4/nonograms/',
+  build: {
+    assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf'],
+    sourcemap: false, // Enables source maps for prod build
+    minify: false, // Disable minification for prod build
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        normalize: path.resolve(__dirname, 'src/styles/modern-normalize.css'),
+        app: path.resolve(__dirname, 'src/scripts/app.js'),
+        view: path.resolve(__dirname, 'src/scripts/view.js'),
+        store: path.resolve(__dirname, 'src/scripts/store.js'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js', // Preserve file names
+      },
+    },
+    cssCodeSplit: true, // Enables CSS code splitting
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}), // Enables autoprefixer
+      ],
+    },
+  },
+});
