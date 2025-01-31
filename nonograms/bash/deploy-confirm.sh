b92dc9bfb14fccde9a206bab65caa672822e7f22
asdqdsa <asdqdsa@protonmail.com>
2025-01-31 22:11:10 +0400

    build: udpate bash script;

--- nonograms/bash/deploy-confirm.sh
+++ nonograms/bash/deploy-confirm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "are you sure you want to deploy? (yes/no): " answer
 if [[ $answer == "yes" ]]; then
-  npm run deploy
+  npm run deploy:force
 else
   echo "deploy cancelled"
 fi
